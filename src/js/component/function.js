9d753ef26d38332ad1ffe0a99231e82a8ac9fecb
Jefferyjasmin <Jefferyjasmin1@gmail.com>
2020-11-05 18:57:41 +0000

    start

---
+++ src/js/component/function.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+const No_name = () => {
+	//for new todo
+	const [userName, setUsername] = useState({});
+	//for our previous todos
+	const [budget, setBudget] = useState([
+		// { todo: "todo 1" },
+    ])
+
+    const [debt,setDebt] = useState({})
+
+    const [expenses,setExpenses] = useState({})
+
+    const [income,setIncome] = useState({})
+}
+
+?
