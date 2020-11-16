143823c5e0ded0aeab84505032f1fdc51dc0ccef
Jefferyjasmin <Jefferyjasmin1@gmail.com>
2020-11-16 00:29:47 +0000

    learning

--- src/js/component/login.js
+++ src/js/component/login.js
@@ -9,83 +9,94 @@ const Login = () => {
 	const [income, setIncome] = useState(0);
 	const history = useHistory();
 	return (
-		<div className="userLogin d-flex justify-content-around ">
-			<div className="imgdiv">
-				<img
-					id="loginImg"
-					src="https://thumbs.dreamstime.com/b/glass-jar-coins-bills-beach-against-sunset-concept-accumulation-money-travel-glass-jar-159993024.jpg"
-				/>
-			</div>
-			<div className="loginRight">
-				<h1>Create Your Account </h1>
+		<div className="userLogin ">
+			<div className="container d-flex">
+				<div className="imgdiv">
+					<img
+						id="loginImg"
+						src="https://thumbs.dreamstime.com/b/glass-jar-coins-bills-beach-against-sunset-concept-accumulation-money-travel-glass-jar-159993024.jpg"
+					/>
+				</div>
+				<div className="loginRight">
+					<h1>Create Your Account </h1>
 
-				<form>
-					<div className="userdiv form-group">
-						<div className="user">
-							<label id="userName">
-								{" "}
-								<b>User Name</b>
-							</label>
-							<i className="far fa-user" />
-							<input
-								type="text"
-								id="userName"
-								placeholder="Username"
-								value={userName}
-								onChange={e => setUserName(e.target.value)}
-							/>
+					<form>
+						<div className="user2">
+							<div className="input-group-prepend">
+								<span className="input-group-text">
+									<i className="far fa-user" />
+								</span>
+							</div>
+							<input type="text" className="form-control" />
 						</div>
-					</div>
 
-					<div className="emaildiv form-group">
-						<div className="email">
-							<label id="staticEmail">
-								{" "}
-								<b>Email</b>
-							</label>
-							<i className="far fa-envelope" />
-							<input
-								type="text"
-								id="staticEmail"
-								placeholder="email@example.com"
-								value={email}
-								onChange={e => setEmail(e.target.value)}
-							/>
+						<div className="userdiv form-group">
+							<div className="user">
+								<label id="userName">
+									{" "}
+									<b>User Name</b>
+								</label>
+								<i className="far fa-user" />
+								<input
+									type="text"
+									id="userName"
+									placeholder="Username"
+									value={userName}
+									onChange={e => setUserName(e.target.value)}
+								/>
+							</div>
 						</div>
-					</div>
-					<div className="incomediv form-group">
-						<div className="incomeField d-flex">
-							<label id="labelincome">
-								<b>Income</b>
-							</label>
-							<input
-								id="userincome"
-								type="number"
-								className="form-control"
-								placeholder="Income"
-								value={income}
-								onChange={e => setIncome(e.target.value)}
-							/>
-							<i className="fas fa-dollar-sign" />
-						</div>
-					</div>
-				</form>
-				{/* <Link to="/home"> */}
-				<button
-					type="button"
-					onClick={async () => {
-						let success = await actions.update(userName, email, parseInt(income));
-						if (success) {
-							history.push("/home");
-						} else {
-							alert("error");
-						}
-					}}>
-					{" "}
-					Submit
-				</button>
 
-				{/* </Link> */}
+						<div className="emaildiv form-group">
+							<div className="email">
+								<label id="staticEmail">
+									{" "}
+									<b>Email</b>
+								</label>
+								<i className="far fa-envelope" />
+								<input
+									type="text"
+									id="staticEmail"
+									placeholder="email@example.com"
+									value={email}
+									onChange={e => setEmail(e.target.value)}
+								/>
+							</div>
+						</div>
+						<div className="incomediv form-group">
+							<div className="incomeField d-flex">
+								<label id="labelincome">
+									<b>Income</b>
+								</label>
+								<input
+									id="userincome"
+									type="number"
+									className="form-control"
+									placeholder="Income"
+									value={income}
+									onChange={e => setIncome(e.target.value)}
+								/>
+								<i className="fas fa-dollar-sign" />
+							</div>
+						</div>
+					</form>
+					{/* <Link to="/home"> */}
+					<button
+						type="button"
+						onClick={async () => {
+							let success = await actions.update(userName, email, parseInt(income));
+							if (success) {
+								history.push("/home");
+							} else {
+								alert("error");
+							}
+						}}>
+						{" "}
+						Submit
+					</button>
+
+					{/* </Link> */}
+				</div>
 			</div>
 		</div>
 	);
