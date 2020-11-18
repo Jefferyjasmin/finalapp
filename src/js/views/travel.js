ac3ad34541d61188a6d2d032a51299f43cb521cc
Jefferyjasmin <Jefferyjasmin1@gmail.com>
2020-11-18 16:14:41 +0000

    traveldone

--- src/js/views/travel.js
+++ src/js/views/travel.js
@@ -3,7 +3,46 @@ const Travel = () => {
 	return (
 		<div className="travelContainer ">
 			<div className="travelBox">
-				<div className="travelItem" />
+				<div className="travelItem1">
+					<h1>Enjoy your trip you earned it </h1>
+					<div className="departureDate" style={{ backgroundColor: "white" }}>
+						<table className="table table-dark" style={{ backgroundColor: "none" }}>
+							<thead>
+								<tr>
+									<th scope="col">#</th>
+									<th scope="col">City</th>
+									<th scope="col">Departure</th>
+									<th scope="col">Return</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th scope="row">1</th>
+									<td>MIA</td>
+									<td>
+										<i className="fas fa-plane-departure" style={{ color: "white" }} />
+										<br />
+										12/02/20 <br />
+										5:11 AM <br />
+										JFKNew York-Kennedy, NY (JFK)
+										<br />
+										Flight number559
+									</td>
+									<td>
+										<i className="fas fa-plane-arrival" style={{ color: "white" }} /> <br />
+										12/16/20
+										<br />
+										7:56 AM
+										<br />
+										PBIWest Palm Beach, FL (PBI)
+										<br />
+										Flight number754
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
