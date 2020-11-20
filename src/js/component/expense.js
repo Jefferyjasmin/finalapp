2c3c38e397300950d724c495794c9ffa5c31ad77
Jefferyjasmin <Jefferyjasmin1@gmail.com>
2020-11-20 15:42:45 +0000

    added photo

--- src/js/component/expense.js
+++ src/js/component/expense.js
@@ -25,12 +25,70 @@ const Expense = () => {
 			<div className="expenseContainer d-flex row">
 				<div className="left col-3">
 					<div className="leftName">
-						{store.userName}
-						User Names
+						<b>
+							{store.userName}
+							User Names
+						</b>
 					</div>
-					<div className="leftBalance">current balance updating</div>
-				</div>
-				<div className="inputField col-9">
+					<div className="leftBalance">
+						<b>current balance updating</b>
+						<div className="expenseContainer1">
+							<label>
+								<b>Enter your expenses</b>
+							</label>
+							<input
+								type="text"
+								id="userExpense"
+								value={expense}
+								placeholder="let add up you expenses"
+								onChange={e => setExpense(e.target.value)}
+								// store.expenses.property: e.target.value
+							/>
+							<input
+								className="userExpense"
+								type="number"
+								id="userExpense"
+								value={value}
+								placeholder="let add up you expenses"
+								onChange={e => setValue(parseInt(e.target.value))}
+								//  store.expenses.value: e.target.value
+							/>
+							<button
+								className="enterExpense btn btn-warning"
+								onClick={() => {
+									const newExp = [...store.expenses];
+									newExp.push({ property: expense, value: value });
+									actions.addExpense(newExp);
+									setExpense("");
+									setValue(0);
+								}}>
+								Enter
+							</button>
+
+							{store.expenses.map((item, index) => {
+								console.log(item);
+								return (
+									<>
+										<button className="btn btn-warning" id="mapli" key={index}>
+											<b>{item.property}</b> <b>{item.value}</b>
+											<button
+												className="btn"
+												type="submit"
+												onClick={() => actions.deleteItem(index)}>
+												{"    "} <i className="fas fa-trash-alt" />
+											</button>
+										</button>
+									</>
+								);
+							})}
+							<Link to="/secondpage">
+								<button className="btn btn-ligh" onClick={() => actions.addExpense(newExpenses)}>
+									save
+								</button>
+							</Link>
+						</div>
+					</div>
+					{/* <div className="inputField col-9">
 					<div className="expenseContainer1">
 						<label> Enter your expenses</label>
 						<input
@@ -76,13 +134,7 @@ const Expense = () => {
 									</span>
 								</>
 							);
-						})}
-					</div>
-
-					{/* <Link to="/secondpage">
-						<button onClick={() => actions.addExpense(newExpenses)}>save</button>
-					</Link>
-					{billing()} */}
+						})} */}
 				</div>
 			</div>
 		</div>
